c2d095f95ab242f60cd313184a9619732ecc8d95
royalcrystalking <martsangel017@gmail.com>
2023-04-12 03:17:00 -0400

    add: filter modal add

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { SearchIcon } from "@chakra-ui/icons";
-import { Button } from "@chakra-ui/react";
 import { BiFilter } from "react-icons/bi";
-// import Modal from 'react-bootstrap/Modal';
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
+import { InputNumber } from 'primereact/inputnumber';
+import { Dropdown } from 'primereact/dropdown';
 
 import styles from "./Filter.module.css";
 
 const Filter = () => {
-  // const [isOpen, setIsOpen] = useState(false);
+  const [ visible, setVisible ] = useState(false);
+  const [ currency1, setCurrency1 ] = useState(0);
+  const [ currency2, setCurrency2 ] = useState(0);
 
   // const handleOpenModal = () => {
   //   setIsOpen(true);
@@ -31,34 +35,44 @@ const Filter = () => {
           />
         </div>
       </div>
-      <Button className={styles.filterBtn}>
+      <button className={styles.filterBtn} onClick={() => setVisible(true)}>
         <BiFilter />
         <span>Filters</span>
-      </Button>
-      {/* <Modal
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        show={isOpen}
-        onHide={() => setIsOpen(false)}
+      </button>
+      <Dialog
+        header="Filter"
+        footer={
+          <div className="w-full">
+            <Button label="Cancel" icon="pi pi-times" size="small" onClick={() => setVisible(false)} />
+          </div>
+        }
+        visible={visible}
+        style={{ width: '800px' }}
+        onHide={() => setVisible(false)}
       >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={() => setIsOpen(false)}>Close</Button>
-        </Modal.Footer>
-      </Modal> */}
+        <h3>Rango de precios</h3>
+        <p>El precio medio por noche es de 177 €</p>
+        <div className={styles.filterBox}>
+          <InputNumber value={currency1} onValueChange={(e) => setCurrency1(e.value)} showButtons mode="currency" currency="EUR" />
+          <div />
+          <InputNumber value={currency2} onValueChange={(e) => setCurrency2(e.value)} showButtons mode="currency" currency="EUR" />
+        </div>
+        <h3>Country</h3>
+        {/* <p>El precio medio por noche es de 177 €</p> */}
+        <div className={styles.filterDropBox}>
+          <Dropdown options={[
+              { name: 'New York', value: 'NY' },
+              { name: 'Rome', value: 'RM' },
+              { name: 'London', value: 'LDN' },
+              { name: 'Istanbul', value: 'IST' },
+              { name: 'Paris', value: 'PRS' }
+          ]}
+          optionLabel="Country" 
+          showClear
+          placeholder="Select a City"
+          />
+        </div>
+      </Dialog>
     </div>
   );
 };
