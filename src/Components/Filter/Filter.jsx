8af909e279d5ff26844dbefb01f2c39d8b85c24d
royalcrystalking <martsangel017@gmail.com>
2023-04-12 03:45:55 -0400

    fix: filter dialog issue fix

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { BiFilter } from "react-icons/bi";
-import { Dialog } from 'primereact/dialog';
-import { Button } from 'primereact/button';
-import { InputNumber } from 'primereact/inputnumber';
-import { Dropdown } from 'primereact/dropdown';
+import { Dialog } from "primereact/dialog";
+import { Button } from "primereact/button";
+import { InputNumber } from "primereact/inputnumber";
+import { Dropdown } from "primereact/dropdown";
 
 import styles from "./Filter.module.css";
 
+const cities = [
+  { name: 'United States', code: 'United States' },
+  { name: 'Japan', code: 'Japan' },
+  { name: 'United Kingdom', code: 'United Kingdom' },
+  { name: 'Australia', code: 'Australia' },
+  { name: 'Mexico', code: 'Mexico' },
+  { name: 'Iceland', code: 'Iceland' }
+];
+
 const Filter = () => {
-  const [ visible, setVisible ] = useState(false);
-  const [ currency1, setCurrency1 ] = useState(0);
-  const [ currency2, setCurrency2 ] = useState(0);
+  const [visible, setVisible] = useState(false);
+  const [currency1, setCurrency1] = useState(0);
+  const [currency2, setCurrency2] = useState(0);
+  const [selectedCity, setSelectedCity] = useState(null);
 
   // const handleOpenModal = () => {
   //   setIsOpen(true);
@@ -43,33 +53,53 @@ const Filter = () => {
         header="Filter"
         footer={
           <div className="w-full">
-            <Button label="Cancel" icon="pi pi-times" size="small" onClick={() => setVisible(false)} />
+            <Button
+              label="Ok"
+              icon="pi pi-check"
+              size="small"
+              onClick={() => setVisible(false)}
+            />
+            <Button
+              label="Cancel"
+              icon="pi pi-times"
+              size="small"
+              onClick={() => setVisible(false)}
+            />
           </div>
         }
         visible={visible}
-        style={{ width: '800px' }}
+        style={{ width: "800px" }}
         onHide={() => setVisible(false)}
       >
-        <h3>Rango de precios</h3>
-        <p>El precio medio por noche es de 177 €</p>
+        <h3>Price range</h3>
+        <p>The average price per night is 177$</p>
         <div className={styles.filterBox}>
-          <InputNumber value={currency1} onValueChange={(e) => setCurrency1(e.value)} showButtons mode="currency" currency="EUR" />
+          <InputNumber
+            value={currency1}
+            onValueChange={(e) => setCurrency1(e.value)}
+            showButtons
+            mode="currency"
+            currency="USD"
+          />
           <div />
-          <InputNumber value={currency2} onValueChange={(e) => setCurrency2(e.value)} showButtons mode="currency" currency="EUR" />
+          <InputNumber
+            value={currency2}
+            onValueChange={(e) => setCurrency2(e.value)}
+            showButtons
+            mode="currency"
+            currency="USD"
+          />
         </div>
         <h3>Country</h3>
-        {/* <p>El precio medio por noche es de 177 €</p> */}
         <div className={styles.filterDropBox}>
-          <Dropdown options={[
-              { name: 'New York', value: 'NY' },
-              { name: 'Rome', value: 'RM' },
-              { name: 'London', value: 'LDN' },
-              { name: 'Istanbul', value: 'IST' },
-              { name: 'Paris', value: 'PRS' }
-          ]}
-          optionLabel="Country" 
-          showClear
-          placeholder="Select a City"
+          <Dropdown
+            value={selectedCity}
+            options={cities}
+            optionLabel="name"
+            editable
+            showClear
+            placeholder="Select a Country"
+            onChange={(e) => setSelectedCity(e.value)}
           />
         </div>
       </Dialog>
